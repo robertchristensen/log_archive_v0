7e72b3df56739dd5e00cd85568b66b81b9ff14a4
Robert Christensen <robert.christensen@gmail.com>
2013-03-07 14:33:38 -0700

    added the code for the optimized kmv estimators.

---
+++ include/kmv_est.h
@@ -0,0 +1,63 @@
+#ifndef KMV_EST_H
+#define KMV_EST_H
+
+#include <string>
+#include <vector>
+#include <inttypes.h>
+
+#include <openssl/md4.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+
+
+/*
+this will do the disticint value estimation found in the paper
+"On Synopses for Distinct-Value Estimation Under Multiset Operations"
+[Kevin Beyer et. al.]
+
+A few specifics about the implementation:
+ * This uses hash values that are 64 bit.
+ * Hash function can easily be changed, but we currently use the first 64 bits
+     of the md4 hash function for speed of calculation.
+ * This creates the set from a character string, grabs q-grams from the string
+     and inserts the values into the estimator.
+*/
+class kmv_est
+{
+    public:
+        kmv_est(const char* str, int k = 60, int q_gram_length = 6);
+        virtual ~kmv_est();
+
+        // get the estimation of the number of distinct values in the set
+        int get_DV();
+
+        // get the estimation of the number of distinct values in the
+        // intersection of this and the other set without creating
+        // a new set
+        int intersection_DV(const kmv_est *val);
+
+        // get the estimation of the number of distinct values in the
+        // union of this set and the other set without creating a
+        // new set
+        int union_DV(const kmv_est *val);
+
+        // estimate teh jaccard value between this set and the
+        // set being passed in.
+        float jaccard_est(const kmv_est *val);
+
+        // gets the estimation of the number of distinct values in the
+        // intersection and union between this set and the set
+        // passed in without creating a new set.
+        void combine_DV(const kmv_est *val, int &intersection_DV, int &union_DV, float &jaccard_est);
+
+        // calculating and creating a set of the intersection or union
+        // can be implemented later when it is found to be important.
+    protected:
+    private:
+        std::vector<uint64_t> *m_kmv_syn;
+        SHA_CTX* mp_hashstruct;  // for creating the object using the hash function
+
+        uint64_t hash(const char* str, int len);
+};
+
+#endif // KMV_EST_H
---
+++ src/kmv_est.cpp
@@ -0,0 +1,180 @@
+#include "../include/kmv_est.h"
+
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+#include <cstring>
+#include <limits>
+
+#include <openssl/md4.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+
+using namespace std;
+
+kmv_est::kmv_est(const char* str, int k, int q_gram_length)
+{
+    m_kmv_syn = new vector<uint64_t>( );
+    mp_hashstruct = (SHA_CTX*) malloc( sizeof(SHA_CTX) );
+
+    unordered_set<uint64_t> contained;
+
+    int strLen = strlen(str); // can optomize here
+    int end_idx = strLen - q_gram_length;
+    uint64_t max_value_in_set = 0; // the max value in the k min value estimator list
+    for(int i = 0; i < end_idx; i++)
+    {
+        uint64_t val = hash(str+i, q_gram_length);
+
+        if(!(contained.count(val) > 0)) // this is true if the value does not already exists in the set
+        {
+            if( m_kmv_syn->size() < k )
+            {
+                m_kmv_syn->push_back(val);
+                push_heap(m_kmv_syn->begin(), m_kmv_syn->end());
+
+                // insert the value into the hash set
+                contained.insert(val);
+                max_value_in_set = m_kmv_syn->front();
+            }else if( val < max_value_in_set )
+            {
+                // insert the new element into kmv
+                m_kmv_syn->push_back(val);
+                push_heap(m_kmv_syn->begin(), m_kmv_syn->end());
+
+                // remove the largest element in kmv from
+                pop_heap(m_kmv_syn->begin(), m_kmv_syn->end());
+                contained.erase(m_kmv_syn->back()); // the last element in the vector will be the largest value.  remove this value from the containes set
+                m_kmv_syn->pop_back();
+
+                // insert the new value into the hash set
+                contained.insert(val);
+                max_value_in_set = m_kmv_syn->front();
+            }
+        }
+    }
+
+    // sort the elements of m_kmv_syn (so it is sorted, not a heap anymore)
+    sort_heap(m_kmv_syn->begin(), m_kmv_syn->end());
+}
+
+kmv_est::~kmv_est()
+{
+    free(mp_hashstruct);
+    delete m_kmv_syn;
+}
+
+
+// get the estimation of the number of distinct values in the set
+int kmv_est::get_DV()
+{
+    // use double.  I don't know if accuricy will be affected by using a float
+    double U = (0.0 + m_kmv_syn->back()) / (sizeof(uint64_t) << 8);
+    return (int) (m_kmv_syn->size() - 1) / U;
+}
+
+// get the estimation of the number of distinct values in the
+// intersection of this and the other set without creating
+// a new set
+int kmv_est::intersection_DV(const kmv_est *val)
+{
+    int int_DV;
+    int un_DV;
+    float jac_est;
+    combine_DV(val, int_DV, un_DV, jac_est);
+
+    return int_DV;
+}
+
+// get the estimation of the number of distinct values in the
+// union of this set and the other set without creating a
+// new set
+int kmv_est::union_DV(const kmv_est *val)
+{
+    int int_DV;
+    int un_DV;
+    float jac_est;
+    combine_DV(val, int_DV, un_DV, jac_est);
+
+    return un_DV;
+}
+
+// estimate teh jaccard value between this set and the
+// set being passed in.
+float kmv_est::jaccard_est(const kmv_est *val)
+{
+    int int_DV;
+    int un_DV;
+    float jac_est;
+    combine_DV(val, int_DV, un_DV, jac_est);
+
+    return jac_est;
+}
+
+// gets the estimation of the number of distinct values in the
+// intersection and union between this set and the set
+// passed in without creating a new set.
+void kmv_est::combine_DV(const kmv_est *val, int &intersection_DV, int &union_DV, float &jaccard_est)
+{
+    // get k = min(k_a, k_b)
+    int min_k = min( this->m_kmv_syn->size(), val->m_kmv_syn->size() );
+
+    // if the number of elemetns in the estimation is too small, no use
+    // trying to calculate everything.  Just say it is invalid.
+    if(min_k < 1)
+    {
+        intersection_DV = 0;
+        union_DV        = 0;
+        jaccard_est     = 0;
+        return;
+    }
+
+    uint64_t last_hash;
+    int current = 0;
+    int intersection_count = 0;
+    vector<uint64_t>::iterator it_a = this->m_kmv_syn->begin();
+    vector<uint64_t>::iterator it_b =  val->m_kmv_syn->begin();
+    // do the operation L_a (+) L_b. In order to calculate the union
+    // estimation, I only need the value of U(k), and k.  I throw
+    // away all other values.
+    while(current < min_k)
+    {
+        current++;
+        if(*it_a < *it_b)
+        {
+            last_hash = *it_a;
+            it_a++;
+        }
+        else if(*it_a > *it_b)
+        {
+            last_hash = *it_b;
+            it_b++;
+        }
+        else
+        {
+            intersection_count++;
+            last_hash = *it_b;
+            it_a++;
+            it_b++;
+        }
+    }
+
+    // first, normalize the value of U(k)
+    double U        = (0.0 + last_hash) / (numeric_limits<uint64_t>::max());
+    union_DV        = (min_k - 1) / U;
+    jaccard_est     = (float) intersection_count / min_k;
+    intersection_DV = jaccard_est * union_DV;
+}
+
+uint64_t kmv_est::hash(const char* str, int len)
+{
+    static unsigned char data[SHA_DIGEST_LENGTH];
+    uint64_t* r_val = (uint64_t*) &data;
+
+    SHA1_Init( mp_hashstruct );
+    SHA1_Update(mp_hashstruct, str, len);
+    SHA1_Final(data, mp_hashstruct);
+
+    return *r_val;
+}
